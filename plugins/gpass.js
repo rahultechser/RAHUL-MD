39c441028a4d0756a0bbfbe6f1dca81d95119948
Rahul Debnath <137095636+rahultechser@users.noreply.github.com>
2024-10-22 01:11:59 +0530

    𝐑𝐚𝐡𝐮𝐥 𝐓𝐞𝐜𝐡 𝐒𝐞𝐫

---
+++ plugins/gpass.js
@@ -0,0 +1,28 @@
+
+/**
+
+//══════════════════════════════════════════════════════════════════════════════\\
+//                                                                                            \\
+//          ██████╗  █████╗ ██╗  ██╗██╗   ██╗██╗         ███╗   ███╗██████╗            \\
+//          ██╔══██╗██╔══██╗██║  ██║██║   ██║██║         ████╗ ████║██╔══██╗          \\
+//          ██████╔╝███████║███████║██║   ██║██║         ██╔████╔██║██║  ██║          \\
+//          ██╔══██╗██╔══██║██╔══██║██║   ██║██║         ██║╚██╔╝██║██║  ██║          \\
+//          ██║  ██║██║  ██║██║  ██║╚██████╔╝███████╗    ██║ ╚═╝ ██║██████╔╝          \\
+//          ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝ ╚══════╝    ╚═╝     ╚═╝╚═════╝            \\
+//                                                                                             \\
+//═══════════════════════════════════════════════════════════════════════════════\\
+
+   * @Project_Name : Rahul-Md
+   * @author : Rahul Tech Ser
+   * @youtube : https://youtube.com/@rahultech009
+   * @description : Rahul-Md ,A Multi-functional whatsapp user bot.
+   * @version : V1
+*
+* 
+   * Created By Rahul Debnath.
+   * © 2024 Rahul-Md.
+*/
+
+
+
+const _0x5086f5=_0x1e44;function _0x1e44(_0x17705f,_0x2c55ec){const _0x4b4991=_0x4b49();return _0x1e44=function(_0x1e442e,_0x382920){_0x1e442e=_0x1e442e-0x1af;let _0x6b55b5=_0x4b4991[_0x1e442e];return _0x6b55b5;},_0x1e44(_0x17705f,_0x2c55ec);}(function(_0x4a75be,_0x1dff95){const _0x56af73=_0x1e44,_0x130a14=_0x4a75be();while(!![]){try{const _0x44b826=-parseInt(_0x56af73(0x1b4))/0x1+-parseInt(_0x56af73(0x1bd))/0x2*(parseInt(_0x56af73(0x1b1))/0x3)+parseInt(_0x56af73(0x1bf))/0x4+parseInt(_0x56af73(0x1b7))/0x5+parseInt(_0x56af73(0x1c2))/0x6*(-parseInt(_0x56af73(0x1b0))/0x7)+-parseInt(_0x56af73(0x1b8))/0x8*(-parseInt(_0x56af73(0x1b5))/0x9)+parseInt(_0x56af73(0x1c3))/0xa;if(_0x44b826===_0x1dff95)break;else _0x130a14['push'](_0x130a14['shift']());}catch(_0x5456bf){_0x130a14['push'](_0x130a14['shift']());}}}(_0x4b49,0xbd223));const crypto=require(_0x5086f5(0x1bc)),{cmd}=require(_0x5086f5(0x1c1));function _0x4b49(){const _0x54a872=['length','7352585vqIAFC','8pZwaXT','other','log','sendMessage','crypto','1730906DWwzUT','randomInt','2501572goBliC','message','../command','2846898xQVWNi','16742050nukYGf','abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+[]{}|;:,.<>?','21PntGVY','3vbJHXJ','Please\x20provide\x20a\x20valid\x20length\x20for\x20the\x20password\x20(Minimum\x2008\x20Characters💦).','🔐\x20*Your\x20Strong\x20Password*\x20🔐\x0a\x0aPlease\x20find\x20your\x20generated\x20password\x20below:\x0a\x0a\x20*BY\x20RAHUL-MD*','802913FaFFKA','867519jpdFnm'];_0x4b49=function(){return _0x54a872;};return _0x4b49();}cmd({'pattern':'gpass','desc':'Generate\x20a\x20strong\x20password.','category':_0x5086f5(0x1b9),'react':'🔐','filename':__filename},async(_0x2436bf,_0x2915e4,_0x3b8927,{from:_0xaa2301,quoted:_0xb8b904,body:_0x3186e3,isCmd:_0x439279,command:_0x40e084,args:_0x4fda1e,q:_0x79da72,isGroup:_0x2d9412,sender:_0x2e731a,senderNumber:_0x5a25de,botNumber2:_0x19134c,botNumber:_0x57a2a9,pushname:_0xdeda21,isMe:_0x2cac8c,isOwner:_0x4f9fc1,groupMetadata:_0x2d7558,groupName:_0x48c652,participants:_0x269847,groupAdmins:_0x52bb94,isBotAdmins:_0x52107b,isAdmins:_0x17ac9b,reply:_0x36c833})=>{const _0x4b79ad=_0x5086f5;try{const _0x3cc80c=_0x4fda1e[0x0]?parseInt(_0x4fda1e[0x0]):0xc;if(isNaN(_0x3cc80c)||_0x3cc80c<0x8)return _0x36c833(_0x4b79ad(0x1b2));const _0x240835=_0x5a2073=>{const _0x4166fb=_0x4b79ad,_0x154974=_0x4166fb(0x1af);let _0x21e5a8='';for(let _0x5ba3ba=0x0;_0x5ba3ba<_0x5a2073;_0x5ba3ba++){const _0x32840a=crypto[_0x4166fb(0x1be)](0x0,_0x154974[_0x4166fb(0x1b6)]);_0x21e5a8+=_0x154974[_0x32840a];}return _0x21e5a8;},_0x4773b0=_0x240835(_0x3cc80c),_0x14b8fe=_0x4b79ad(0x1b3);await _0x2436bf[_0x4b79ad(0x1bb)](_0xaa2301,{'text':_0x14b8fe},{'quoted':_0x2915e4}),await _0x2436bf['sendMessage'](_0xaa2301,{'text':_0x4773b0},{'quoted':_0x2915e4});}catch(_0x4e9c0f){console[_0x4b79ad(0x1ba)](_0x4e9c0f),_0x36c833('❌\x20Error\x20generating\x20password🤕:\x20'+_0x4e9c0f[_0x4b79ad(0x1c0)]);}});
