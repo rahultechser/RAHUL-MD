6300e0305e79e268688db2716d03e695579040a6
Rahul Debnath <137095636+rahultechser@users.noreply.github.com>
2024-10-22 00:32:52 +0530

    𝐑𝐚𝐡𝐮𝐥 𝐓𝐞𝐜𝐡 𝐒𝐞𝐫

--- plugins/ai.js
+++ plugins/ai.js
@@ -1,1 +1,28 @@
-kk
+
+/**
+
+//══════════════════════════════════════════════════════════════════════════════\\
+//                                                                                            \\
+//          ██████╗  █████╗ ██╗  ██╗██╗   ██╗██╗         ███╗   ███╗██████╗            \\
+//          ██╔══██╗██╔══██╗██║  ██║██║   ██║██║         ████╗ ████║██╔══██╗          \\
+//          ██████╔╝███████║███████║██║   ██║██║         ██╔████╔██║██║  ██║          \\
+//          ██╔══██╗██╔══██║██╔══██║██║   ██║██║         ██║╚██╔╝██║██║  ██║          \\
+//          ██║  ██║██║  ██║██║  ██║╚██████╔╝███████╗    ██║ ╚═╝ ██║██████╔╝          \\
+//          ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝ ╚══════╝    ╚═╝     ╚═╝╚═════╝            \\
+//                                                                                             \\
+//═══════════════════════════════════════════════════════════════════════════════\\
+
+   * @Project_Name : Rahul-Md
+   * @author : Rahul Tech Ser
+   * @youtube : https://youtube.com/@rahultech009
+   * @description : Rahul-Md ,A Multi-functional whatsapp user bot.
+   * @version : V1
+*
+* 
+   * Created By Rahul Debnath.
+   * © 2024 Rahul-Md.
+*/
+
+
+
+function _0x57a9(_0x4a1dc0,_0x52db63){const _0x5c948f=_0x5c94();return _0x57a9=function(_0x57a900,_0x32cea9){_0x57a900=_0x57a900-0xa4;let _0x30af4f=_0x5c948f[_0x57a900];return _0x30af4f;},_0x57a9(_0x4a1dc0,_0x52db63);}const _0x2f93a4=_0x57a9;(function(_0x39c65a,_0x20958e){const _0x257b57=_0x57a9,_0x23da36=_0x39c65a();while(!![]){try{const _0x588a1f=-parseInt(_0x257b57(0xa6))/0x1+-parseInt(_0x257b57(0xad))/0x2*(parseInt(_0x257b57(0xa5))/0x3)+-parseInt(_0x257b57(0xb0))/0x4+-parseInt(_0x257b57(0xb1))/0x5*(parseInt(_0x257b57(0xaf))/0x6)+-parseInt(_0x257b57(0xa9))/0x7+parseInt(_0x257b57(0xae))/0x8*(parseInt(_0x257b57(0xac))/0x9)+parseInt(_0x257b57(0xb2))/0xa*(parseInt(_0x257b57(0xab))/0xb);if(_0x588a1f===_0x20958e)break;else _0x23da36['push'](_0x23da36['shift']());}catch(_0x560da1){_0x23da36['push'](_0x23da36['shift']());}}}(_0x5c94,0xbb9c8));const config=require('../config'),{cmd,commands}=require(_0x2f93a4(0xa8)),{fetchJson}=require('../lib/functions');function _0x5c94(){const _0x25919b=['411368JZVueA','7705128ZZVkRb','1374792GdXVwj','105592HUCYwR','10vKWtUt','2740hVWHOd','https://chatgptforprabath-md.vercel.app/api/gptv1?q=','3nFclNI','687523yYzwqk','data','../command','5997222MFKtXS','ai\x20chat.','81895TFTWLR','9qdPfaG'];_0x5c94=function(){return _0x25919b;};return _0x5c94();}cmd({'pattern':'ai','react':'📑','desc':_0x2f93a4(0xaa),'category':'main','filename':__filename},async(_0x209623,_0x5df140,_0x162d97,{from:_0x10e5cc,quoted:_0x444cbb,body:_0x5cbd25,isCmd:_0x328372,command:_0x2eb1eb,args:_0xc5e4fc,q:_0x7ec851,isGroup:_0x30328e,sender:_0x14d8b3,senderNumber:_0x56a79e,botNumber2:_0x286c86,botNumber:_0x31c990,pushname:_0x1847fa,isMe:_0x4528ef,isOwner:_0x2d1b69,groupMetadata:_0x540be0,groupName:_0x30e798,participants:_0x3daceb,groupAdmins:_0x5c3857,isBotAdmins:_0x2332ad,isAdmins:_0x1a9aa0,reply:_0x54adf0})=>{const _0x2544aa=_0x2f93a4;try{let _0x30e599=await fetchJson(_0x2544aa(0xa4)+_0x7ec851);return _0x54adf0(''+_0x30e599[_0x2544aa(0xa7)]);}catch(_0x7447f6){console['log'](_0x7447f6),_0x54adf0(''+_0x7447f6);}});
