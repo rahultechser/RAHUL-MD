1d4e966a68656afb27df4050a34610d99e663d6d
Rahul Debnath <137095636+rahultechser@users.noreply.github.com>
2024-10-22 01:25:31 +0530

    𝐑𝐚𝐡𝐮𝐥 𝐓𝐞𝐜𝐡 𝐒𝐞𝐫

---
+++ plugins/loli.js
@@ -0,0 +1,28 @@
+
+/**
+
+//══════════════════════════════════════════════════════════════════════════════\\
+//                                                                                            \\
+//          ██████╗  █████╗ ██╗  ██╗██╗   ██╗██╗         ███╗   ███╗██████╗            \\
+//          ██╔══██╗██╔══██╗██║  ██║██║   ██║██║         ████╗ ████║██╔══██╗          \\
+//          ██████╔╝███████║███████║██║   ██║██║         ██╔████╔██║██║  ██║          \\
+//          ██╔══██╗██╔══██║██╔══██║██║   ██║██║         ██║╚██╔╝██║██║  ██║          \\
+//          ██║  ██║██║  ██║██║  ██║╚██████╔╝███████╗    ██║ ╚═╝ ██║██████╔╝          \\
+//          ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝ ╚══════╝    ╚═╝     ╚═╝╚═════╝            \\
+//                                                                                             \\
+//═══════════════════════════════════════════════════════════════════════════════\\
+
+   * @Project_Name : Rahul-Md
+   * @author : Rahul Tech Ser
+   * @youtube : https://youtube.com/@rahultech009
+   * @description : Rahul-Md ,A Multi-functional whatsapp user bot.
+   * @version : V1
+*
+* 
+   * Created By Rahul Debnath.
+   * © 2024 Rahul-Md.
+*/
+
+
+
+const _0x221dce=_0x45f4;function _0x45f4(_0x3aeefc,_0x38de5a){const _0x7b62d6=_0x7b62();return _0x45f4=function(_0x45f435,_0x21178a){_0x45f435=_0x45f435-0x1d8;let _0x491e24=_0x7b62d6[_0x45f435];return _0x491e24;},_0x45f4(_0x3aeefc,_0x38de5a);}(function(_0x575b39,_0x2f9a0f){const _0x43f30d=_0x45f4,_0x39ff70=_0x575b39();while(!![]){try{const _0x316e54=-parseInt(_0x43f30d(0x1e1))/0x1+-parseInt(_0x43f30d(0x1dc))/0x2*(-parseInt(_0x43f30d(0x1e7))/0x3)+parseInt(_0x43f30d(0x1e4))/0x4*(parseInt(_0x43f30d(0x1db))/0x5)+-parseInt(_0x43f30d(0x1d8))/0x6+-parseInt(_0x43f30d(0x1e0))/0x7+parseInt(_0x43f30d(0x1dd))/0x8+parseInt(_0x43f30d(0x1e8))/0x9;if(_0x316e54===_0x2f9a0f)break;else _0x39ff70['push'](_0x39ff70['shift']());}catch(_0x5d6270){_0x39ff70['push'](_0x39ff70['shift']());}}}(_0x7b62,0x6a129));function _0x7b62(){const _0x255500=['1682352dLKuIx','message','Fetch\x20a\x20random\x20anime\x20girl\x20image.','85ukJOEs','6520jiNpZd','6914424sFHZmC','url','axios','5564748KepSvh','572701hOHbRk','lolii','*Error\x20Fetching\x20Anime\x20Girl\x20image*:\x20','27460BlPAKc','fun','../command','786AmzTBO','2226618UgbrCy','sendMessage','👸\x20*RAHUL-MD\x20RANDOM\x20ANIME\x20GIRL\x20IMAGES*\x20👸\x0a\x0a\x0a\x20*🧬©RAHUL-MD\x20BY\x20RAHUL\x20TECH\x20SER*','get'];_0x7b62=function(){return _0x255500;};return _0x7b62();}const axios=require(_0x221dce(0x1df)),{cmd,commands}=require(_0x221dce(0x1e6));cmd({'pattern':'loli','alias':[_0x221dce(0x1e2)],'desc':_0x221dce(0x1da),'category':_0x221dce(0x1e5),'react':'🐱','filename':__filename},async(_0x30cb9a,_0x287326,_0x2d9072,{from:_0xd4b79,quoted:_0x18f211,body:_0x1a9f88,isCmd:_0x426484,command:_0x36e2b0,args:_0x22427c,q:_0x23a6f8,isGroup:_0x1e4d81,sender:_0x15e31b,senderNumber:_0x2aa437,botNumber2:_0xffbdd5,botNumber:_0x5a9dc8,pushname:_0x549e96,isMe:_0x1c8ab8,isOwner:_0x46352b,groupMetadata:_0x429a3f,groupName:_0x1d6c40,participants:_0x232a2e,groupAdmins:_0x117174,isBotAdmins:_0x547122,isAdmins:_0x1808ca,reply:_0x43823f})=>{const _0x3a2641=_0x221dce;try{const _0x272938='https://api.waifu.pics/sfw/waifu',_0x3d5a13=await axios[_0x3a2641(0x1eb)](_0x272938),_0x527b7c=_0x3d5a13['data'];await _0x30cb9a[_0x3a2641(0x1e9)](_0xd4b79,{'image':{'url':_0x527b7c[_0x3a2641(0x1de)]},'caption':_0x3a2641(0x1ea)},{'quoted':_0x287326});}catch(_0x1f41cc){console['log'](_0x1f41cc),_0x43823f(_0x3a2641(0x1e3)+_0x1f41cc[_0x3a2641(0x1d9)]);}});
