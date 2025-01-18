eff2e0d64f976f261c41b1b955e196d7014e3372
Rahul Debnath <137095636+rahultechser@users.noreply.github.com>
2025-01-19 00:01:45 +0530

    Update afk.js

--- plugins/afk.js
+++ plugins/afk.js
@@ -1,65 +1,26 @@
-const { System } = require('../lib/');
-const { secondsToHms } = require("./client/"); 
 
-let AFK = {
-	isAfk: false,
-	reason: false,
-	lastseen: 0
-};
+/**
 
-System({
-	on: 'all',
-	fromMe: false
-}, async (message, match) => {
-	if(message.isBot) return;
-	if(message.fromMe) return;
-	if (!AFK.isAfk)  return;
-	if(!message.mention.isBotNumber && !message.quoted && message.isGroup)  return;
-	if (message.mention.isBotNumber && message.isGroup) {
-   	    await message.send('```This is a bot. My owner is not here at the moment```\n' +
-		              (AFK.reason !== false ? '\n*Reason:* ```' + AFK.reason + '```' : '') +
-		    	      (AFK.lastseen !== 0 ? '\n*Last Seen:* ```' + secondsToHms(Math.round((new Date()).getTime() / 1000) - AFK.lastseen) + '```' : ''), {
-			      quoted: message.data
-			    });
-	} else if (message.isGroup && message.reply_message.sender == message.user.jid) {
-	    await message.send('```This is a bot. My owner is not here at the moment```\n' +
-			      (AFK.reason !== false ? '\n*Reason:* ```' + AFK.reason + '```' : '') +
-			      (AFK.lastseen !== 0 ? '\n*Last Seen:* ```' + secondsToHms(Math.round((new Date()).getTime() / 1000) - AFK.lastseen) + '```' : ''), {
-			      quoted: message.data
-			   });
+//══════════════════════════════════════════════════════════════════════════════\\
+//                                                                                            \\
+//          ██████╗  █████╗ ██╗  ██╗██╗   ██╗██╗         ███╗   ███╗██████╗            \\
+//          ██╔══██╗██╔══██╗██║  ██║██║   ██║██║         ████╗ ████║██╔══██╗          \\
+//          ██████╔╝███████║███████║██║   ██║██║         ██╔████╔██║██║  ██║          \\
+//          ██╔══██╗██╔══██║██╔══██║██║   ██║██║         ██║╚██╔╝██║██║  ██║          \\
+//          ██║  ██║██║  ██║██║  ██║╚██████╔╝███████╗    ██║ ╚═╝ ██║██████╔╝          \\
+//          ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝ ╚══════╝    ╚═╝     ╚═╝╚═════╝            \\
+//                                                                                             \\
+//═══════════════════════════════════════════════════════════════════════════════\\
 
-	} else if(!message.isGroup) {
-	    await message.send('```This is a bot. My owner is not here at the moment```\n' +
-				(AFK.reason !== false ? '\n*Reason:* ```' + AFK.reason + '```' : '') +
-				(AFK.lastseen !== 0 ? '\n*Last Seen:* ```' + secondsToHms(Math.round((new Date()).getTime() / 1000) - AFK.lastseen) + '```' : ''), {
-				quoted: message.data
-			      });
-	}
-});
+   * @Project_Name : Rahul-Md
+   * @author : Rahul Tech Ser
+   * @youtube : https://youtube.com/@rahultech009
+   * @description : Rahul-Md ,A Multi-functional whatsapp user bot.
+   * @version : V1
+*
+* 
+   * Created By Rahul Debnath.
+   * © 2025 Rahul-Md.
+*/
 
-
-System({
-	on: 'text',
-	fromMe: true
-}, async (message, match) => {
-	if(message.isBot) return;
-	if(message.sender !== message.user.jid) return;
-	if (!AFK.isAfk)  return;
-	AFK.lastseen = 0;
-	AFK.reason = false;
-	AFK.isAfk = false;
-	await message.send('```I am not AFK anymore!```');
-});
-
-System({
-	pattern: 'afk ?(.*)',
-	fromMe: true,
-	desc: 'away from keyboard'
-}, async (message, match) => {
-	if (AFK.isAfk) return;
-        if(message.isBot) return;
-	AFK.lastseen = Math.round((new Date()).getTime() / 1000);
-	if (match !== '') AFK.reason = match;
-	AFK.isAfk = true;
-	await message.send(AFK.reason ? '*_Im AFK now!_*\n*Reason:* ' + AFK.reason :  '*_Im AFK now!_*');
-});
+const _0x860338=_0x4aff;(function(_0x4ef1aa,_0x50e18b){const _0x4e2a1f=_0x4aff,_0x383afa=_0x4ef1aa();while(!![]){try{const _0x5b4861=parseInt(_0x4e2a1f(0x131))/0x1+parseInt(_0x4e2a1f(0x126))/0x2+-parseInt(_0x4e2a1f(0x139))/0x3*(parseInt(_0x4e2a1f(0x11b))/0x4)+parseInt(_0x4e2a1f(0x13a))/0x5+-parseInt(_0x4e2a1f(0x11a))/0x6+-parseInt(_0x4e2a1f(0x121))/0x7+parseInt(_0x4e2a1f(0x138))/0x8;if(_0x5b4861===_0x50e18b)break;else _0x383afa['push'](_0x383afa['shift']());}catch(_0xca22c0){_0x383afa['push'](_0x383afa['shift']());}}}(_0x42be,0x34882));function _0x42be(){const _0x1f548f=['```','126714EIoodo','send','isAfk','\x0a*Reason:*\x20```','./client/','mention','jid','4447944QLdpuP','21LRjroN','251105nJwmml','1007436rdohpG','44332WaXlbr','reason','getTime','round','isBot','lastseen','2450252Ivrswk','away\x20from\x20keyboard','fromMe','reply_message','text','155530pjrOVR','../lib/','\x0a*Last\x20Seen:*\x20```','isGroup','```This\x20is\x20a\x20bot.\x20My\x20owner\x20is\x20not\x20here\x20at\x20the\x20moment```\x0a','isBotNumber','data','```I\x20am\x20not\x20AFK\x20anymore!```','sender','quoted'];_0x42be=function(){return _0x1f548f;};return _0x42be();}function _0x4aff(_0x1b61e9,_0x70eced){const _0x42be8c=_0x42be();return _0x4aff=function(_0x4affec,_0x58444f){_0x4affec=_0x4affec-0x11a;let _0x390187=_0x42be8c[_0x4affec];return _0x390187;},_0x4aff(_0x1b61e9,_0x70eced);}const {System}=require(_0x860338(0x127)),{secondsToHms}=require(_0x860338(0x135));let AFK={'isAfk':![],'reason':![],'lastseen':0x0};System({'on':'all','fromMe':![]},async(_0xc3f360,_0x51a4e6)=>{const _0x3ae084=_0x860338;if(_0xc3f360[_0x3ae084(0x11f)])return;if(_0xc3f360[_0x3ae084(0x123)])return;if(!AFK[_0x3ae084(0x133)])return;if(!_0xc3f360[_0x3ae084(0x136)][_0x3ae084(0x12b)]&&!_0xc3f360[_0x3ae084(0x12f)]&&_0xc3f360[_0x3ae084(0x129)])return;if(_0xc3f360[_0x3ae084(0x136)][_0x3ae084(0x12b)]&&_0xc3f360[_0x3ae084(0x129)])await _0xc3f360[_0x3ae084(0x132)](_0x3ae084(0x12a)+(AFK[_0x3ae084(0x11c)]!==![]?_0x3ae084(0x134)+AFK[_0x3ae084(0x11c)]+_0x3ae084(0x130):'')+(AFK[_0x3ae084(0x120)]!==0x0?_0x3ae084(0x128)+secondsToHms(Math[_0x3ae084(0x11e)](new Date()[_0x3ae084(0x11d)]()/0x3e8)-AFK[_0x3ae084(0x120)])+_0x3ae084(0x130):''),{'quoted':_0xc3f360[_0x3ae084(0x12c)]});else{if(_0xc3f360[_0x3ae084(0x129)]&&_0xc3f360[_0x3ae084(0x124)][_0x3ae084(0x12e)]==_0xc3f360['user'][_0x3ae084(0x137)])await _0xc3f360[_0x3ae084(0x132)]('```This\x20is\x20a\x20bot.\x20My\x20owner\x20is\x20not\x20here\x20at\x20the\x20moment```\x0a'+(AFK['reason']!==![]?'\x0a*Reason:*\x20```'+AFK[_0x3ae084(0x11c)]+'```':'')+(AFK['lastseen']!==0x0?_0x3ae084(0x128)+secondsToHms(Math[_0x3ae084(0x11e)](new Date()[_0x3ae084(0x11d)]()/0x3e8)-AFK['lastseen'])+_0x3ae084(0x130):''),{'quoted':_0xc3f360[_0x3ae084(0x12c)]});else!_0xc3f360['isGroup']&&await _0xc3f360[_0x3ae084(0x132)](_0x3ae084(0x12a)+(AFK[_0x3ae084(0x11c)]!==![]?'\x0a*Reason:*\x20```'+AFK[_0x3ae084(0x11c)]+'```':'')+(AFK[_0x3ae084(0x120)]!==0x0?_0x3ae084(0x128)+secondsToHms(Math[_0x3ae084(0x11e)](new Date()[_0x3ae084(0x11d)]()/0x3e8)-AFK[_0x3ae084(0x120)])+'```':''),{'quoted':_0xc3f360[_0x3ae084(0x12c)]});}}),System({'on':_0x860338(0x125),'fromMe':!![]},async(_0x3f7a73,_0x2bff08)=>{const _0x3d3b00=_0x860338;if(_0x3f7a73['isBot'])return;if(_0x3f7a73['sender']!==_0x3f7a73['user'][_0x3d3b00(0x137)])return;if(!AFK[_0x3d3b00(0x133)])return;AFK[_0x3d3b00(0x120)]=0x0,AFK[_0x3d3b00(0x11c)]=![],AFK['isAfk']=![],await _0x3f7a73[_0x3d3b00(0x132)](_0x3d3b00(0x12d));}),System({'pattern':'afk\x20?(.*)','fromMe':!![],'desc':_0x860338(0x122)},async(_0x59af0a,_0x4d0363)=>{const _0x272750=_0x860338;if(AFK['isAfk'])return;if(_0x59af0a[_0x272750(0x11f)])return;AFK[_0x272750(0x120)]=Math[_0x272750(0x11e)](new Date()[_0x272750(0x11d)]()/0x3e8);if(_0x4d0363!=='')AFK['reason']=_0x4d0363;AFK['isAfk']=!![],await _0x59af0a['send'](AFK[_0x272750(0x11c)]?'*_Im\x20AFK\x20now!_*\x0a*Reason:*\x20'+AFK[_0x272750(0x11c)]:'*_Im\x20AFK\x20now!_*');});
